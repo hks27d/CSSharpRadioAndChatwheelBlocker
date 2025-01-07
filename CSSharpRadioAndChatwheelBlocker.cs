92689987ad7a29186de5822e4f39c8e79c6c60b6
HKS 27D <51793958+hks27d@users.noreply.github.com>
2025-01-07 03:52:11 +0000

    Initial release

---
+++ CSSharpRadioAndChatwheelBlocker.cs
@@ -0,0 +1,60 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Commands;
+
+namespace CSSharpRadioAndChatwheelBlocker
+{
+    public class CSSharpRadioAndChatwheelBlocker : BasePlugin
+    {
+        public override string ModuleName => "CS# Radio & Chatwheel Blocker";
+        public override string ModuleVersion => "1.0.0";
+        public override string ModuleAuthor => "HKS 27D";
+        public override string ModuleDescription => "";
+
+        private readonly string[] RadioArray = [
+            "coverme",
+            "takepoint",
+            "holdpos",
+            "regroup",
+            "followme",
+            "takingfire",
+            "go",
+            "fallback",
+            "sticktog",
+            "getinpos",
+            "stormfront",
+            "report",
+            "roger",
+            "enemyspot",
+            "needbackup",
+            "sectorclear",
+            "inposition",
+            "reportingin",
+            "getout",
+            "negative",
+            "enemydown",
+            "sorry",
+            "cheer",
+            "compliment",
+            "thanks",
+            "go_a",
+            "go_b",
+            "needrop",
+            "deathcry"
+        ];
+
+        public override void Load(bool hotReload)
+        {
+            AddCommandListener("playerchatwheel", CommandListener_RadioAndChatwheelBlocker);
+            for (int i = 0; i < RadioArray.Length; i++)
+            {
+                AddCommandListener(RadioArray[i], CommandListener_RadioAndChatwheelBlocker);
+            }
+        }
+
+        private HookResult CommandListener_RadioAndChatwheelBlocker(CCSPlayerController? player, CommandInfo info)
+        {
+            return HookResult.Handled;
+        }
+    }
+};
---
+++ README.md
@@ -0,0 +1,28 @@
+[![Discord](https://img.shields.io/discord/1323042994437357599?style=for-the-badge)](https://discord.com/invite/zsmUzthPXx)
+
+# 📌 CSSharp Radio & Chatwheel Blocker
+Stop Radio and Chatwheel on the server.
+
+## 🌐 Description
+This is just a basic plugin to stop Radio and Chatwheel.
+
+## 📗 Dependecies
+- [CounterStrikeSharp](https://github.com/roflmuffin/CounterStrikeSharp)
+
+## 📁 Installation
+1. Get an IDE & Code Editor (Visual Studio, Rider or others).
+2. Install package 'Microsoft.Extensions.Logging.Abstractions'.
+3. Edit and debug if you need to.
+4. Build the code.
+5. Add the files (`.dll`, `.pdb`, `.deps.json`) in their own directory.
+6. Rename directory. Use the same name.
+7. Upload directory in `/game/csgo/addons/counterstrikesharp/plugins`
+
+## 🤝 Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## ⭐ Show your support
+Give a ⭐ if this project helped you.
+
+## 📝 License
+MIT based on outlined exemption from CounterStrikeSharp.
